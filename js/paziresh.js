8a1ead2e39e9e9ff047610a1c24d062625cb18f6
Unknown <kooshan@me.com>
2017-08-17 20:53:27 +0430

    Initial commit

---
+++ js/paziresh.js
@@ -0,0 +1,35 @@
+// JavaScript Document
+(function() {  
+    var dialog = document.getElementById('window');  
+    document.getElementById('createItButton').onclick = function() {  
+        dialog.show();  
+    };  
+    document.getElementById('exit').onclick = function() {  
+        dialog.close();  
+    };  
+})();  
+
+
+
+// JavaScript Document
+(function() {  
+    var dialog = document.getElementById('nobat');  
+    document.getElementById('reserveButton').onclick = function() {  
+        dialog.show();  
+    };  
+    document.getElementById('exit').onclick = function() {  
+        dialog.close();  
+    };  
+})();  
+
+
+// JavaScript Document
+(function() {  
+    var dialog = document.getElementById('cost');  
+    document.getElementById('calcButton').onclick = function() {  
+        dialog.show();  
+    };  
+    document.getElementById('exit').onclick = function() {  
+        dialog.close();  
+    };  
+})();  
